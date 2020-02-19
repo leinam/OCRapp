05b4f2de866474037be6835be7f91ef530361f49
Leina <leinameoli@gmail.com>
2020-02-19 20:39:37 +0300

    Second Commit. Added some functionality to take images using the phone's camera. Activity layout design.

--- src/main/java/com/leina/ocrapp/MainActivity.java
+++ src/main/java/com/leina/ocrapp/MainActivity.java
@@ -1,31 +1,67 @@
 package com.leina.ocrapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
 
 public class MainActivity extends AppCompatActivity {
+    // initialize layout objects
     ImageView imageView;
     TextView textView;
+    Button btn_capture;
+    Button btn_detect;
+    Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // map to layout objects in xml layout
         imageView = findViewById(R.id.imageView_thumbnail);
         textView = findViewById(R.id.textView_detected);
+        btn_detect.setEnabled(false);
+
+
+        //when you click capture function to take photo is called
+        btn_capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent(); //call function to use camera app to add
+                btn_detect.setEnabled(true); // allow text detection
+            }
+        });
+
+        //
+        btn_detect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
 
     }
 
 
-    // from google dev documentation on site
+    //retrieved from google dev documentation on site
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private void dispatchTakePictureIntent() {
@@ -35,12 +71,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //load thumbnail onto our activity's image view for preview
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+            imageBitmap = (Bitmap) extras.get("data");//convert captured image to bitmap
+            imageView.setImageBitmap(imageBitmap);// preview image in app
         }
+    }
+
+
+    private void detectImageText(){
+        //generate from bitmap object created from photo taken
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+
+        btn_detect.setEnabled(false);// while processing
+
+        recognizer.processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                btn_detect.setEnabled(true);
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                btn_capture.setEnabled(true);
+                e.printStackTrace(); // show error in logs
+            }
+        });
     }
 }
